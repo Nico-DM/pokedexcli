1d90c10bfc6b374433856dfd36d642337deafbf3
Nico-DM <nicomgaletto@gmail.com>
2026-04-19 23:10:11 -0300

    refactor: replace main loop with startRepl function for command handling

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		cleanInput := cleanInput(input)
-		fmt.Printf("Your command was: %v\n", cleanInput[0])
-	}
+	startRepl()
 }
